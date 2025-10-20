30294f3ced19233fb067653c791bed81af19a18e
wi23261 <wi23261@lehre.dhbw-stuttgart.de>
2025-10-21 00:55:49 +0200

    create skadisHole class and loop to create a bunch of objects

--- pegboard-configurator/src/main.js
+++ pegboard-configurator/src/main.js
@@ -1,25 +1,33 @@
 import paper from 'paper';
 
+class skadisHole {
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
+
+    // Skadis hole measurements:
+    var holeWidth = 50; // 5mm
+    var holeHeight = 150; // 15mm
+    var holeRadius = holeWidth / 2;
+
+    this.path = new paper.Path.Rectangle(
+      new paper.Rectangle(this.x, this.y, holeWidth, holeHeight),
+      holeRadius
+    );
+  }
+};
+
 // Setup Paper.js on the canvas
 const canvas = document.getElementById('myCanvas');
 paper.setup(canvas);
 
-// Create a Paper.js Path to draw a line
-const path = new paper.Path();
-path.strokeColor = 'black';
-const start = new paper.Point(100, 100);
-path.moveTo(start);
-path.lineTo(start.add([100, -50]));
 
+for (var i = 0; i < 10; i++) {
+  const tempPath = new skadisHole(100 * i, 100).path;
+  tempPath.strokeColor = 'black';
+  paper.project.activeLayer.addChild(tempPath);
+}
 
-
-var myPoint = new paper.Point(120, 120);
-var mySize = new paper.Size(200, 100);
-var myRectangle = new paper.Rectangle(myPoint, mySize);
-
-path.lineTo(myPoint);
-const secondPath = new paper.Path.Rectangle(myRectangle);
-secondPath.strokeColor = "green";
-secondPath.fillColor = "blue";
+//path.smooth();
 // Draw the view now
 paper.view.draw();
