0c846d865b58ebcee42db820e935786c086a7306
wi23261 <wi23261@lehre.dhbw-stuttgart.de>
2025-10-20 21:46:48 +0200

    humble beginnings: draw line on screen

--- pegboard-configurator/src/main.js
+++ pegboard-configurator/src/main.js
@@ -1,24 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import paper from 'paper';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Setup Paper.js on the canvas
+const canvas = document.getElementById('myCanvas');
+paper.setup(canvas);
 
-setupCounter(document.querySelector('#counter'))
+// Create a Paper.js Path to draw a line
+const path = new paper.Path();
+path.strokeColor = 'black';
+const start = new paper.Point(100, 100);
+path.moveTo(start);
+path.lineTo(start.add([100, -50]));
+
+// Draw the view now
+paper.view.draw();
