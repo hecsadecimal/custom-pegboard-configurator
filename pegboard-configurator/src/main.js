a4c6479627fc778802d5ee7c1dc5bbb0c069cccb
wi23261 <wi23261@lehre.dhbw-stuttgart.de>
2025-10-21 01:45:38 +0200

    add function to add create a grid of holes, adjust size to fit canvas

--- pegboard-configurator/src/main.js
+++ pegboard-configurator/src/main.js
@@ -6,27 +6,37 @@ class skadisHole {
     this.y = y;
 
     // Skadis hole measurements:
-    var holeWidth = 50; // 5mm
-    var holeHeight = 150; // 15mm
-    var holeRadius = holeWidth / 2;
+    this.holeWidth = 5; // 5mm
+    this.holeHeight = 15; // 15mm
+    this.holeRadius = this.holeWidth / 2;
 
     this.path = new paper.Path.Rectangle(
-      new paper.Rectangle(this.x, this.y, holeWidth, holeHeight),
-      holeRadius
+      new paper.Rectangle(this.x, this.y, this.holeWidth, this.holeHeight),
+      this.holeRadius
     );
   }
+  createGrid(numberOfRows, numberOfColumns) {
+    for (var i = 0; i < numberOfRows; i++) {
+      for (var j = 0; j < numberOfColumns; j++) {
+        const tempPath = new paper.Path.Rectangle(
+          new paper.Rectangle(this.x + 40 * j, this.y + 40 * i, this.holeWidth, this.holeHeight),
+          this.holeRadius
+        );
+        tempPath.strokeColor = 'black';
+        paper.project.activeLayer.addChild(tempPath);
+      }
+    }
+  }
 };
+
 
 // Setup Paper.js on the canvas
 const canvas = document.getElementById('myCanvas');
 paper.setup(canvas);
 
 
-for (var i = 0; i < 10; i++) {
-  const tempPath = new skadisHole(100 * i, 100).path;
-  tempPath.strokeColor = 'black';
-  paper.project.activeLayer.addChild(tempPath);
-}
+var path = new skadisHole(100, 100);
+path.createGrid(10, 10);
 
 //path.smooth();
 // Draw the view now
