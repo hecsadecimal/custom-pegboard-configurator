80d8b24c4b3fe588f757922026188a6876bbfc83
wi23261 <wi23261@lehre.dhbw-stuttgart.de>
2025-10-20 23:42:09 +0200

    draw rectangle

--- pegboard-configurator/src/main.js
+++ pegboard-configurator/src/main.js
@@ -11,5 +11,15 @@ const start = new paper.Point(100, 100);
 path.moveTo(start);
 path.lineTo(start.add([100, -50]));
 
+
+
+var myPoint = new paper.Point(120, 120);
+var mySize = new paper.Size(200, 100);
+var myRectangle = new paper.Rectangle(myPoint, mySize);
+
+path.lineTo(myPoint);
+const secondPath = new paper.Path.Rectangle(myRectangle);
+secondPath.strokeColor = "green";
+secondPath.fillColor = "blue";
 // Draw the view now
 paper.view.draw();
